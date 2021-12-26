5748db0b7a5f6c00843c1376ca3d402a08253167
wilmarques <wileymarques@live.com>
2021-12-26 12:33:47 +0000

    Generate container app

---
+++ apps/container/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'container',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transform: {
+    '^.+\\.[tj]s$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/container',
+};
